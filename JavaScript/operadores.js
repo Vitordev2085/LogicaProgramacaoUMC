02c4309950320440eca422513a6819189f23ff59
Vitordev2085 <vitorhongawa20@gmail.com>
2025-03-25 10:43:08 -0300

    12

---
+++ JavaScript/operadores.js
@@ -0,0 +1,33 @@
+//Operadores Aritméticos
+
+let n1 = 4
+let n2 = 2
+
+//Operador soma
+let soma = n1 + n2
+console.log(soma)
+
+
+//Operador subtracao
+let subtracao = n1 - n2
+console.log(subtracao)
+
+//Operador Multiplicacao
+let multiplicacao = n1 * n2
+console.log(multiplicacao)
+
+//Operador divisao
+let divisao = n1 / n2
+console.log(divisao)
+
+//Operador Modulo
+let modulo = n1 % n2
+console.log(modulo);
+
+//Operador de potencia
+let potencia = Math.pow(n1,n2)
+console.log(potencia)
+
+//Operador raiz quadrada
+let raizQuadrada = Math.sqrt(n1)
+console.log(raizQuadrada);
